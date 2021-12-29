1dba54703e2fed5b9631aba616f3416206fb3e2e
Bharti Thakur <jadaun50@gmail.com>
2021-12-29 15:02:12 +0530

    Images Api Hit .

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+import 'Network/Api/ApiHandler.dart';
+import 'Network/Model/ResponseListImages/ResponsePixabayImage.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -12,6 +15,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  ResponsePixabayImage? responsePixabayImage;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    _hitImagesApi();
+  }
+
+  void _hitImagesApi()async{
+    var response = await ApiHandler.getImages();
+    if(response != null){
+      responsePixabayImage = response;
+      print("bharti response -> $response");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
 
@@ -20,24 +40,25 @@ class _HomePageState extends State<HomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: new StaggeredGridView.countBuilder(
-          crossAxisCount: 4,
-          itemCount: 8,
-          itemBuilder: (BuildContext context, int index) => new Container(
-              color: Colors.green,
-              child: new Center(
-                child: new CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: new Text('$index'),
-                ),
-              )),
-          staggeredTileBuilder: (int index) =>
-          new StaggeredTile.count(2, index.isEven ? 2 : 1),
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-        )
+      body: Column(
+        children: [
+          Text("lenght is : ${responsePixabayImage?.hits?.length}"),
+          Container(
+            child: StaggeredGridView.countBuilder(
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              itemCount: responsePixabayImage?.hits?.length,
+              itemBuilder: (BuildContext context, int index) {
+                var hitData = responsePixabayImage?.hits![index];
+                return Image.network(hitData?.previewURL ?? "");
+                },
+              staggeredTileBuilder: (int index) =>
+              new StaggeredTile.count(1, index.isEven ? 1.2 : 1.5),
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+            ),
+          ),
+        ],
       ),
     );
   }
